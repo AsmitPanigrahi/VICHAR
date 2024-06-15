82697c508d5f317c53af80d3660af9777a5bba38
Asmit Panigrahi <128639526+AsmitPanigrahi@users.noreply.github.com>
2024-06-16 00:27:03 +0530

    chore: Refactor LogoutBtn component and add loading spinner

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,21 +1,72 @@
-import React from 'react'
+// import React, { useState } from 'react'
+// import {useDispatch} from 'react-redux'
+// import authService from '../../appwrite/auth'
+// import {logout} from '../../store/authSlice'
+
+// function LogoutBtn() {
+//     const dispatch = useDispatch()
+//     const logoutHandler = () => {
+//         authService.logout().then(() => {
+//             dispatch(logout())
+//         })
+//     }
+//   return (
+//     <button
+//     className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+//     onClick={logoutHandler}
+//     >Logout</button>
+//   )
+// }
+
+// export default LogoutBtn
+
+
+
+
+
+import React, { useState } from 'react'
 import {useDispatch} from 'react-redux'
 import authService from '../../appwrite/auth'
 import {logout} from '../../store/authSlice'
+import { useNavigate } from "react-router-dom";
+import { ImSpinner2 } from "react-icons/im";
 
 function LogoutBtn() {
-    const dispatch = useDispatch()
-    const logoutHandler = () => {
-        authService.logout().then(() => {
-            dispatch(logout())
-        })
-    }
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState(false);
+
+  const LogoutBtnHandler = () => {
+    setLoader(true);
+    authService.logout().then(() => {
+      dispatch(logout());
+      navigate("/login");
+      setLoader(false);
+    });
+  };
   return (
     <button
-    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-    onClick={logoutHandler}
-    >Logout</button>
-  )
+      onClick={LogoutBtnHandler}
+      className=" hover:text-black hover:font-bold px-3 py-2 rounded-md"
+    >
+      {loader ? (
+        <div className="flex items-center justify-center gap-3">
+          <h1>Logout</h1>
+          <ImSpinner2 className="animate-spin mx-auto" />
+        </div>
+      ) : (
+        "Logout"
+      )}
+    </button>
+  );
 }
 
 export default LogoutBtn
+
+//I had used const LogoutBtn instead of function LogOutBtn.
+
+// In JavaScript, "hoisting" is a behavior where variables and function declarations are moved, 
+// or "hoisted," to the top of their containing scope before the code is executed. 
+// This means that you can use functions and variables before you actually declare them in your code.
+
+
