a438404e638833811f630107be61b6716db7f68b
Asmit Panigrahi <128639526+AsmitPanigrahi@users.noreply.github.com>
2024-06-17 10:42:56 +0530

    Updated TinyMCE API key

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Editor } from '@tinymce/tinymce-react';
 import {Controller } from 'react-hook-form';
+import conf from '../conf/conf';
 
 
 export default function RTE({name, control, label, defaultValue =""}) {
@@ -13,7 +14,7 @@ export default function RTE({name, control, label, defaultValue =""}) {
     control={control}
     render={({field: {onChange}}) => (
         <Editor
-        apiKey='28bheefnnese6h7uhueamot70fifvr8fyd3zcia65w48l4b1'
+        apiKey={conf.tinyMce}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
